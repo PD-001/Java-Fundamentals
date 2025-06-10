688e6b1897762481dd2e73b25eed83bbbea15011
PD-001 <110840658+PD-001@users.noreply.github.com>
2025-06-10 11:05:46 +0530

    Fifth Assisted Problem Solution

---
+++ Assisted05.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Assisted05{
+	public static void main(String [] args){
+		Scanner sc=new Scanner(System.in);
+		int radius=sc.nextInt();
+		int height=sc.nextInt();
+		double Volume = 3.14 * radius * radius * height;
+		System.out.println(Volume);
+	}
+}
